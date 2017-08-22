05b3b89639294582f2fad2ee4fed824bc3284cf3
zagrebelin <pavel@zagrebelin.ru>
2017-08-22 11:58:30 +0500

    fix setup.py file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='liqui',
+    name='zabbix',
     version='1.0.0',
     description='Async Zabbix api wrapper',
     long_description=open('README.md', 'rt').read(),
-    url='https://github.com/banteg/liqui',
-    author='banteg',
+    url='https://github.com/zagrebelin/zabbix',
+    author='zagrebelin',
     license='MIT',
-    py_modules=['liqui'],
+    py_modules=['zabbix'],
     install_requires=['aiohttp']
 )
