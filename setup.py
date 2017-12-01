0aee82ae80802cfdd22cfabcf304dcdc2da8c188
Pavel Zagrebelin <pavel@zagrebelin.ru>
2017-12-01 13:33:20 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='zabbix',
+    name='aiozabbix',
     version='1.0.0',
     description='Async Zabbix api wrapper',
     long_description=open('README.md', 'rt').read(),
     url='https://github.com/zagrebelin/aiozabbix',
     author='zagrebelin',
     license='MIT',
-    py_modules=['zabbix'],
+    py_modules=['aiozabbix'],
     install_requires=['aiohttp']
 )
